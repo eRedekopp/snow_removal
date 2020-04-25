b630f76515d98aea8112227ebbd2603494752de5
eRedekopp <eric.redekopp@mail.com>
2020-04-24 21:27:06 -0600

    Started thinking about state representation

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 # The main file in the project
+
+import numpy as np
+
+DEFAULT_SNOW_DEPTH = 5
+
+# Tree search
+#   State:
+#      - current location of shoveller
+#      - current snow levels
+#   Transition:
+#      - move to an adjacent square
+#      - snow moves from current square to destination square
+#   Goal:
+#      - All "driveway" tiles have snow levels of 0
+
+def generate_driveway(width, height):
+    x = np.ones((width + 2, height), float) * DEFAULT_SNOW_DEPTH
+    return x
+
